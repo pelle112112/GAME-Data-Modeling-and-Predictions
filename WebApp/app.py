3cdc721009098d49f297a2e0757e9f5174ec0e9b
Pelle Hald Vedsmand <cph-pv73@cphbusiness.dk>
2024-12-11 12:23:14 +0100

    Changed folder structure

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,16 +1,21 @@
 import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
+import os
 
 
 
 st.set_page_config(layout= "wide")
 
-nav = get_nav_from_toml("../streamlitapp/pages_sections.toml")
-
-
-pg = st.navigation(nav)
-
-
-add_page_title(pg)
-
-pg.run()
+toml_path = "streamlitapp/pages_sections.toml"
+#nav = get_nav_from_toml("streamlitapp/pages_sections.toml")
+if not os.path.exists(toml_path):
+    st.error("Navigation file not found")
+else:
+    nav = get_nav_from_toml(toml_path)
+    
+    if nav:
+        pg = st.navigation(nav)
+        add_page_title(pg)
+        pg.run()
+    else:
+        st.error("Navigation object is empty, check the format of the toml file")
