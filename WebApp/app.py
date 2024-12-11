f6c9a67e66942329996a2af1ddbd7480b50757ff
Pelle Hald Vedsmand <cph-pv73@cphbusiness.dk>
2024-12-11 12:15:40 +0100

    Fixed paths

--- WebApp/app.py
+++ WebApp/app.py
@@ -2,9 +2,11 @@ import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
 
+
 st.set_page_config(layout= "wide")
 
 nav = get_nav_from_toml("../streamlitapp/pages_sections.toml")
+
 
 pg = st.navigation(nav)
 
